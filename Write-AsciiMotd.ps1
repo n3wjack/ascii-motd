2be3144da7f7d8c3aee8a190a3d0f37d99e6064d
n3wjack <n3wjack@n3wjack.net>
2022-08-20 18:27:48 +0200

    Add ASCII art MOTD script.

---
+++ Write-AsciiMotd.ps1
@@ -0,0 +1,18 @@
+$baseUrl = "https://www.asciiart.eu"
+
+$categories = "animals", "art-and-design", "books", "buildings-and-places", "cartoons", "clothing-and-accessories", "comics", "computers", "electronics", "food-and-drinks", "holiday-and-events", "logos", "miscellaneous", "movies", "music", "mythology", "nature", "people", "plants", "religion", "space", "sports-and-outdoors", "television", "toys", "vehicles", "video-games", "weapons"
+
+$category = $categories | random
+# Get the category homepage.
+$progressPreference = 'silentlyContinue'
+$r = Invoke-WebRequest "$baseUrl/$category" -UseBasicParsing
+
+$subcategories = $r.Links | where { $_.Href -like "/$category/*" }
+$url = "$baseUrl/$($subcategories.Href | random)" 
+$r = Invoke-WebRequest $url
+# Get all PRE tags, excluding the ones for the page header.
+$element = $r.ParsedHtml.getElementsByTagName("PRE") | where { $_.className -notlike "*text-dark*" } | random
+
+Write-Host "`n$($element.innerText)" -ForegroundColor Cyan
+Write-Host "`n  from: $url"
+
