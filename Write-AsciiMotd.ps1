c854a3e28c6ea9513cfaf40a5cb67bc9a4340501
n3wjack <n3wjack@n3wjack.net>
2022-08-20 20:41:30 +0200

    Add color parameter.

--- Write-AsciiMotd.ps1
+++ Write-AsciiMotd.ps1
@@ -1,3 +1,5 @@
+param ([System.ConsoleColor]$Color=[System.ConsoleColor]::Cyan)
+
 $baseUrl = "https://www.asciiart.eu"
 
 $categories = "animals", "art-and-design", "books", "buildings-and-places", "cartoons", "clothing-and-accessories", "comics", "computers", "electronics", "food-and-drinks", "holiday-and-events", "logos", "miscellaneous", "movies", "music", "mythology", "nature", "people", "plants", "religion", "space", "sports-and-outdoors", "television", "toys", "vehicles", "video-games", "weapons"
@@ -8,11 +10,11 @@ $progressPreference = 'silentlyContinue'
 $r = Invoke-WebRequest "$baseUrl/$category" -UseBasicParsing
 
 $subcategories = $r.Links | where { $_.Href -like "/$category/*" }
-$url = "$baseUrl/$($subcategories.Href | random)" 
+$url = "$baseUrl$($subcategories.Href | random)" 
 $r = Invoke-WebRequest $url
 # Get all PRE tags, excluding the ones for the page header.
-$element = $r.ParsedHtml.getElementsByTagName("PRE") | where { $_.className -notlike "*text-dark*" } | random
+$element = $r.ParsedHtml.getElementsByTagName("PRE") | where { $_.className -notlike "*text-dark*" } | Get-Random
 
-Write-Host "`n$($element.innerText)" -ForegroundColor Cyan
-Write-Host "`n  from: $url"
+Write-Host "`n$($element.innerText)" -ForegroundColor $Color
+Write-Host "`n  from: $url" -ForegroundColor DarkGray
 
