0c72e64157c24a6af3564bfb7a5d207a97f3b5cc
n3wjack <n3wjack@n3wjack.net>
2022-08-22 21:34:10 +0200

    Refactor to fix multiple category level issue.

--- Write-AsciiMotd.ps1
+++ Write-AsciiMotd.ps1
@@ -1,20 +1,44 @@
+[CmdletBinding()]
 param ([System.ConsoleColor]$Color=[System.ConsoleColor]::Cyan)
 
 $baseUrl = "https://www.asciiart.eu"
 
 $categories = "animals", "art-and-design", "books", "buildings-and-places", "cartoons", "clothing-and-accessories", "comics", "computers", "electronics", "food-and-drinks", "holiday-and-events", "logos", "miscellaneous", "movies", "music", "mythology", "nature", "people", "plants", "religion", "space", "sports-and-outdoors", "television", "toys", "vehicles", "video-games", "weapons"
 
-$category = $categories | random
+
 # Get the category homepage.
 $progressPreference = 'silentlyContinue'
-$r = Invoke-WebRequest "$baseUrl/$category" -UseBasicParsing
 
-$subcategories = $r.Links | where { $_.Href -like "/$category/*" }
-$url = "$baseUrl$($subcategories.Href | random)" 
-$r = Invoke-WebRequest $url
-# Get all PRE tags, excluding the ones for the page header.
-$element = $r.ParsedHtml.getElementsByTagName("PRE") | where { $_.className -notlike "*text-dark*" } | Get-Random
+function Get-AsciiArt ($category) 
+{
+    $result = $null
+    $categories = "/$category"
 
-Write-Host "`n$($element.innerText)" -ForegroundColor $Color
-Write-Host "`n  from: $url" -ForegroundColor DarkGray
+    do {
+        $url = "$baseUrl$categories"
+        $r = Invoke-WebRequest $url
+
+        $subcategories = $r.Links | where { $_.Href -like "$categories*" }
+
+        if ($subcategories.Count -eq 0) {
+            $result = $r.ParsedHtml.getElementsByTagName("PRE") | where { $_.className -notlike "*text-dark*" } | Get-Random
+        } else {
+            $categories = $subcategories.Href | Get-Random
+        }
+    } while (($result -eq $null) -and ($subcategories.Count -gt 0))
+
+    if ($result -ne $null) {
+        $art = $result.innerText
+    } else {
+        $art = ""
+    }
+
+    return $art, $url
+}
+
+$category = $categories | Get-Random
+$result = Get-AsciiArt $category
+
+Write-Host "`n$($result[0])" -ForegroundColor $Color
+Write-Host "`n  from: $($result[1])" -ForegroundColor DarkGray
 
