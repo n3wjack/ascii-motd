72d6c86b21d3e40a90816b86ae44a79b9b85e078
n3wjack <n3wjack@n3wjack.net>
2022-08-23 21:24:39 +0200

    Fix issue, remove TODO.
    Fix issue where the same page is used as the subcategory page, because
    the URL also matched the search pattern.
    Add Write-Verbose statements for debugging.
    Remove TODO file.

--- TODO.md
+++
@@ -1,4 +0,0 @@
-
-- fix extra subfolder art: https://www.asciiart.eu/buildings-and-places/furniture  
-
-
--- Write-AsciiMotd.ps1
+++ Write-AsciiMotd.ps1
@@ -10,7 +10,7 @@
 #>
 
 [CmdletBinding()]
-param ([System.ConsoleColor]$Color=[System.ConsoleColor]::Cyan)
+param ([System.ConsoleColor]$Color=[System.ConsoleColor]::Cyan, [string]$Category)
 
 $baseUrl = "https://www.asciiart.eu"
 $categories = "animals", "art-and-design", "books", "buildings-and-places", "cartoons", "clothing-and-accessories", "comics", "computers", "electronics", "food-and-drinks", "holiday-and-events", "logos", "miscellaneous", "movies", "music", "mythology", "nature", "people", "plants", "religion", "space", "sports-and-outdoors", "television", "toys", "vehicles", "video-games", "weapons"
@@ -26,12 +26,17 @@ function Get-AsciiArt ($category)
         $url = "$baseUrl$categories"
         $r = Invoke-WebRequest $url
 
-        $subcategories = $r.Links | where { $_.Href -like "$categories*" }
+        $subcategories = $r.Links | where { $_.Href -like "$categories/*" }
+
+        Write-Verbose "Found $($subcategories.Count) subcategories: $($subcategories.Href)"
 
         if ($subcategories.Count -eq 0) {
-            $result = $r.ParsedHtml.getElementsByTagName("PRE") | where { $_.className -notlike "*text-dark*" } | Get-Random
+            $preElements = $r.ParsedHtml.getElementsByTagName("PRE") | where { $_.className -notlike "*text-dark*" } 
+            Write-Verbose "Found $($preElements.Count) PRE elements."
+            $result = $preElements | Get-Random
         } else {
             $categories = $subcategories.Href | Get-Random
+            Write-Verbose "Categories set to '$categories'."
         }
     } while (($result -eq $null) -and ($subcategories.Count -gt 0))
 
@@ -44,8 +49,11 @@ function Get-AsciiArt ($category)
     return $art, $url
 }
 
-$category = $categories | Get-Random
-$result = Get-AsciiArt $category
+if ($Category -eq "") {
+    $Category = $categories | Get-Random
+}
+
+$result = Get-AsciiArt $Category
 
 Write-Host "`n$($result[0])" -ForegroundColor $Color
 Write-Host "`n  from: $($result[1])" -ForegroundColor DarkGray
